06309b753a7a9ea907da570503f308881ab86a91
Shikari King <108302860+shikariking16@users.noreply.github.com>
2023-11-22 18:07:56 -0800

    Fixed the grid and added the chess pieces in a folder.

--- chess.js
+++ chess.js
@@ -8,24 +8,25 @@ let ChessTable = document.createElement('table');
 // Loop to create rows and cells
 for (let i = 0; i < 8; i++) {
     let tr = document.createElement('tr');
-    let rankLabel = 8 - i; // Generate rank numbers (8, 7, 6...) for the left side
 
     for (let j = 0; j < 8; j++) {
         let td = document.createElement('td');
 
-        // Create file labels (a, b, c...) for the bottom row
-        if (i === 7 ) {
-            let fileLabel = String.fromCharCode(96 + j); // Generate file letters (a, b, c...) for the bottom
-            td.textContent = fileLabel;
-            td.setAttribute('class', 'fileLabel');
-        }
-
-        // Add rank numbers (8, 7, 6...) to the left side
-        if (j === 0) {
-            td.textContent = rankLabel;
-            td.setAttribute('class', 'rankLabel');
-            rankLabel++;
-        }
+       // Create file labels (a, b, c...) for the bottom row and add rank numbers (8, 7, 6...) to the left side
+    if (i === 7 && j === 0) {
+        // For the bottom-left cell (first cell in the last row)
+        td.textContent = '1a'; // Set '1a' for the bottom-left cell
+        td.setAttribute('class', 'fileLabel rankLabel');
+    } else if (i === 7) {
+        // For the bottom row (excluding the bottom-left cell)
+        let fileLabel = String.fromCharCode(97 + j); // Generate file letters (a, b, c...) for the bottom
+        td.textContent = fileLabel;
+        td.setAttribute('class', 'fileLabel');
+    } else if (j === 0) {
+        // For the leftmost column (excluding the bottom-left cell)
+        td.textContent = 8 - i; // Set rank numbers (8, 7, 6...) to the left side
+        td.setAttribute('class', 'rankLabel');
+    }
 
         // Add alternating cell colors
         if ((i + j) % 2 == 0) {
