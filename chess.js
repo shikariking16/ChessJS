b5bcb857bdd55264a1fb456873a712c667869af0
Shikari King <108302860+shikariking16@users.noreply.github.com>
2023-11-22 17:01:49 -0800

    Fixed the board, it was 8x9 now its 8x8 also added the ranks and files

--- chess.js
+++ chess.js
@@ -1,5 +1,3 @@
-// chess.js
-
 // Create the chessboard container element
 let chessContainer = document.createElement('div');
 chessContainer.setAttribute('id', 'ChessContainer');
@@ -10,21 +8,36 @@ let ChessTable = document.createElement('table');
 // Loop to create rows and cells
 for (let i = 0; i < 8; i++) {
     let tr = document.createElement('tr');
+    let rankLabel = 8 - i; // Generate rank numbers (8, 7, 6...) for the left side
 
     for (let j = 0; j < 8; j++) {
-    let td = document.createElement('td');
+        let td = document.createElement('td');
 
-    if ((i + j) % 2 == 0) {
-        td.setAttribute('class', 'cell whitecell');
-        
-    }else {
-        td.setAttribute('class', 'cell blackcell');
+        // Create file labels (a, b, c...) for the bottom row
+        if (i === 7 ) {
+            let fileLabel = String.fromCharCode(96 + j); // Generate file letters (a, b, c...) for the bottom
+            td.textContent = fileLabel;
+            td.setAttribute('class', 'fileLabel');
+        }
+
+        // Add rank numbers (8, 7, 6...) to the left side
+        if (j === 0) {
+            td.textContent = rankLabel;
+            td.setAttribute('class', 'rankLabel');
+            rankLabel++;
+        }
+
+        // Add alternating cell colors
+        if ((i + j) % 2 == 0) {
+            td.setAttribute('class', 'cell whitecell');
+        } else {
+            td.setAttribute('class', 'cell blackcell');
+        }
+
+        tr.appendChild(td);
     }
-    tr.appendChild(td);
 
-  }
-
-  ChessTable.appendChild(tr);
+    ChessTable.appendChild(tr);
 }
 
 // Append the chessboard table to the container
